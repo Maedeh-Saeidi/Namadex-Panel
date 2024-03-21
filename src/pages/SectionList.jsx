ea0c184b33334df4dde8f71a6954fa4555abcba6
Maedeh <Maedeh@NGN.local>
2024-03-21 23:03:27 +0330

    Displaying items in table

--- src/pages/SectionList.jsx
+++ src/pages/SectionList.jsx
@@ -13,23 +13,41 @@ import "../fonts/IRANSans 400.ttf";
 export default function SectionList() {
   const [sections, setSections] = useState("");
   const [isLoading, setIsLoading] = useState(false);
+  const [imageUrls, setImageUrls] = useState([]);
+  const [images, setImages] = useState([]);
 
   useEffect(() => {
-    const configuration = {
-      method: "Get",
-      url: "https://api.namadex.ir/api/v1/section",
-    };
-    axios(configuration)
-      .then((result) => {
-        setSections(result.data.data);
-        console.log(sections);
+    const fetchData = async () => {
+      try {
+        const sectionApi = "https://api.namadex.ir/api/v1/section";
+        const getSections = await axios.get(sectionApi);
+        const allSections = getSections.data.data;
+        setSections(allSections);
+
+        const imageUrls = [];
+        for (const section of allSections) {
+          const sectionId = section.id;
+          const imageApi = `https://api.namadex.ir/api/v1/section/${sectionId}/image`;
+          const imageResponse = await axios.get(imageApi);
+          const imageUrl = imageResponse.config.url;
+          imageUrls.push(imageUrl);
+        }
+        setImages(imageUrls);
         setIsLoading(true);
-      })
-      .catch((Response) => {
-        console.log(Response.message);
-      });
+      } catch (error) {
+        console.error("Error fetching data", error);
+      }
+    };
+    fetchData();
   }, []);
 
+  {
+    isLoading && console.log(images);
+  }
+
+  const handleEditClick = (id) => {
+    console.log("Edit button clicked for id:", id);
+  };
   return (
     <TableContainer component={Paper} sx={{ padding: "30px" }}>
       <Table sx={{ minWidth: 650 }} aria-label="simple table">
@@ -44,7 +62,7 @@ export default function SectionList() {
         </TableHead>
         <TableBody>
           {isLoading &&
-            sections.map((row) => (
+            sections.map((row, index) => (
               <TableRow
                 key={row.id}
                 sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
@@ -52,16 +70,25 @@ export default function SectionList() {
                 <TableCell component="th" scope="row">
                   {row.id}
                 </TableCell>
+
                 <TableCell sx={{ fontFamily: "font-IRANSans" }} align="left">
                   {row.title}
                 </TableCell>
-
                 <TableCell sx={{ fontFamily: "font-IRANSans" }} align="left">
                   {row.description}
                 </TableCell>
-                <TableCell align="left"></TableCell>
                 <TableCell>
-                  <Button style={{ textTransform: "lowercase" }}>edit</Button>
+                  {<img src={imageUrls[index]} alt={`Image ${index}`} />}
+                </TableCell>
+
+                <TableCell>
+                  <Button
+                    key={row.id}
+                    style={{ textTransform: "lowercase" }}
+                    onClick={() => handleEditClick(row.id)}
+                  >
+                    edit
+                  </Button>
                 </TableCell>
               </TableRow>
             ))}
